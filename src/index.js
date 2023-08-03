1e53be8c3af4e923b3a902b10756d04ae8813f4b
Dheeraj Sharma <dheerajsksd40@gmail.com>
2023-08-03 23:43:40 +0530

    "Ready to deploy"

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
